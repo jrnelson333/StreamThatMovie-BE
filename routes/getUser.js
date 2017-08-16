7b34e511930b5b7cfddbe77c7d2cbcfd223b49ca
jordan nelson <jordannelson@jordans-MacBook-Pro.local>
2017-08-15 22:36:58 -0400

    Add condition to getUser

--- routes/getUser.js
+++ routes/getUser.js
@@ -1,6 +1,11 @@
 var getUser = function getUser(req, res) {
-    var user = req.user;
-    res.json(user);
+    if (req.user) {
+        var user = {id: req.user._id, email: req.user.email};
+        return res.status(200).send(user);
+    } else {
+        return res.status(403).send();
+    }
+
 }
 
 module.exports = getUser;
