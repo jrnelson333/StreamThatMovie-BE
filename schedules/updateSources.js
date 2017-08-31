a7a4ae236d89577456768541acb2e8eb9d23a91b
jordan nelson <jordannelson@jordans-MacBook-Pro.local>
2017-08-30 20:02:35 -0400

    update

--- schedules/updateSources.js
+++ schedules/updateSources.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
 const queryNetflix = require('./util').queryNetflix
 
-var scheduleUpdateNetflix = schedule.scheduleJob('45 5 * * *', function () {
+var scheduleUpdateNetflix = schedule.scheduleJob('45 0 * * *', function () {
     queryNetflix()
 });
